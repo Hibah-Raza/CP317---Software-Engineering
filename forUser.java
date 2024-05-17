5150e5f47e385a52bcd8e6f33d7f1afed764da36
hibah <hibahraza@outlook.com>
2024-05-17 00:35:22 -0400

    added comments to forUser class

--- forUser.java
+++ forUser.java
@@ -1,22 +1,62 @@
-public class forUser {
-    
-    private String name;
-    private SmartHome smartHome;
+"""
+-----------------------------------------------------------------------------
+This class (forUser) represents the user in the Smart Home Automation System.
+It will allow our user to be able to control the lights + the temp settings as
+well as be able to set automation rules through the app.
+-----------------------------------------------------------------------------
+"""
+public class forUser { //Open class
 
-    public forUser(String name, SmartHome, smartHome){
+    private String name; //Name of the user
+    private SmartHome smartHome; //SHAS (Smart Home Automation System) assigned to user
+
+    """
+    -----------------------------------------------
+    Constructing a forUser object that is specified 
+    with the user's name and Smart Home System.
+    -----------------------------------------------
+    parameters: name, smartHome
+    -----------------------------------------------
+    """
+    public forUser(String name, SmartHome, smartHome) { //Open forUser
         this.name = name;
         this.smartHome = smartHome;
-    }
+    } //Close forUser
 
-    public void lightControl(boolean stat){
+    """
+    -----------------------------------------------
+    Will control the lights through the Smart Home
+    System, for turning on and off.
+    -----------------------------------------------
+    parameters: stat - true = on/false = off
+    -----------------------------------------------
+    """
+    public void lightControl(boolean stat) { //Open lightControl
         smartHome.lightControl(stat);
-    }
+    } //Close lightControl
 
-    public void thermostatControl(int temp){
+    """
+    -----------------------------------------------
+    Will control the temperature through the Smart
+    Home System, for high or low temperature.
+    -----------------------------------------------
+    parameters: temp - what temperature is set to
+    -----------------------------------------------
+    """
+    public void thermostatControl(int temp) { //Open thermostatControl
         smartHome.thermostatControl(temp);
-    }
+    } //Close thermostatControl
 
-    public void setAutomation(setAutomation rule){
+    """
+    -----------------------------------------------
+    Will set an rule for automation through the
+    Smart Home System, for automation part of SHAS
+    -----------------------------------------------
+    parameters: rule
+    -----------------------------------------------
+    """
+    public void setAutomation(setAutomation rule) { //Open setAutomation
         smartHome.addAutomation(rule);
-    }
-}
+    } //Close setAutomation
+
+} //Close class
