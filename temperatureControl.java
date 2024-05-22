6311036d4c4d8ab0eb21a4e5181a1335a970a221
Rafay3855 <159960739+Rafay3855@users.noreply.github.com>
2024-05-21 21:56:18 -0400

    Update temperatureControl.java

--- temperatureControl.java
+++ temperatureControl.java
@@ -1,5 +1,28 @@
 //for Abdul/Rafay to work on
 
-/*public class TemperatureControl{
-    
-}*/
+public class TemperatureControl 
+{
+    private int temperature;
+
+    public TemperatureControl() 
+    {
+        this.temperature = 20; // Default temperature
+    }
+
+    public void temperatureControl(int temp) 
+    {
+        if (temp < 15 || temp > 30) 
+        {
+            System.out.println("Temperature is out of range. Please enter a temperature between 15 and 30.");
+        } else 
+        {
+            this.temperature = temp;
+            System.out.println("Temperature set to " + temp + " degrees.");
+        }
+    }
+
+    public int getTemperature() 
+    {
+        return this.temperature;
+    }
+}
