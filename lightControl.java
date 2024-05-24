8826360cf60d7c430ba261c946c05d58383811d7
aarezs <aarezsiddiqui@gmail.com>
2024-05-23 21:10:36 -0400

    Update lightControl.java

--- lightControl.java
+++ lightControl.java
@@ -1,5 +1,21 @@
-//for Aarez to work on
+//for Aarez to work on (yay ^.^)
 
-/*public class LightControl{
+/*
+-----------------------------------------------------------------------------
+This class represents the Light Control System in the Smart Home Automation
+System. It allows the user to control the lights by turning them on or off
+and to check the current status of the lights.
+-----------------------------------------------------------------------------
+*/
+
+public class LightControl {
+    private boolean status;
     
-}*/
+    public void setLightStatus(boolean status) {
+        this.status = status; 
+        System.out.println("Light is now " + (status ? "ON" : "OFF"));
+    }
+    public boolean getLightStatus() {
+        return status;
+    }
+}
