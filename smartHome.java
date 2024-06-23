e8babf1ebd3bb6b392be9c754983660a807f56c7
AliannoTul <aliannotul@gmail.com>
2024-06-23 14:21:52 -0400

    fixing syntax

--- smartHome.java
+++ smartHome.java
@@ -2,74 +2,74 @@
 import java.util.ArrayList;
 import java.util.List;
 
-"""
+/*
 -----------------------------------------------------------------------------
 - Placeholder description
 This class assigns preexisting lightControl, temperatureControl, and AutomationRule
 objects to a single smartHome
 -----------------------------------------------------------------------------
-"""
+*/
 public class SmartHome {
     private LightControl lightControl = new LightControl();
-    private temperatureControl tempControl = new temperatureControl();
+    private temperatureControl temperature_Control = new temperatureControl();
     private List<AutomationRule> automationList;
 
-    """
+    /*
     -----------------------------------------------------------------------------
     Assigns lightControl and temperatureControl objects to the SmartHome object
     -----------------------------------------------------------------------------
     parameters: lightControl, temperatureControl
     -----------------------------------------------------------------------------
-    """
-    public SmartHome(LightControl lightControl, temperatureControl tempControl) {
-        this.lightControl = lightControl;
-        this.temperatureControl = tempControl;
+    */
+    public SmartHome(LightControl lightCtrl, temperatureControl temperatureCtrl) {
+        this.lightControl = lightCtrl;
+        this.temperature_Control = temperatureCtrl;
         this.automationList = new ArrayList<>();
     }
     
-    """
+    /*
     -----------------------------------------------------------------------------
     Controls the status of the light object
     -----------------------------------------------------------------------------
     parameters: boolean status - the status value to send the lightControl object
                                     - true = on, false = off
     -----------------------------------------------------------------------------
-    """
+    */
     public void lightControl(boolean status) {
         this.lightControl.setLightStatus(status);
     }
 
-    """
+    /*
     -----------------------------------------------------------------------------
     Controls the temperature of the thermostat
     -----------------------------------------------------------------------------
     parameters: int temp - the temperature to send to the thermostat object
     -----------------------------------------------------------------------------
-    """
-    public void thermostatControl (int temp) {
-        this.temperatureControl.setTemperature(temp );
+    */
+    public void temperatureControl (int temp) {
+        this.temperature_Control.setTemperature(temp);
     }
 
 
     
 
 
-    """ 
+    /*
     -----------------------------------------------------------------------------
     Adds automations to an ArrayList of automations, the automations can decide to run on their own, 
     this just organizes them together somewhere that the Smart Home can access them.
     -----------------------------------------------------------------------------
     parameters: rule - any automation that falls under the AutomationRule class or subclasses
     -----------------------------------------------------------------------------
-    """
+    */
     public void addAutomationRule (AutomationRule auto) {
-        automationList.add(auto);
+        this.automationList.add(auto);
     }
 
 
     // CHECK IF THIS WORKS
     public void displayAutomationList () {
-        for (int i : this.automationList()) {
+        for (AutomationRule i : this.automationList) {
             i.displayAutomation();
         }
     }
