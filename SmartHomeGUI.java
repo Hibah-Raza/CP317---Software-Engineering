b054ee6a93b7c08f0c7cd47d1cf35d65986525e5
kruugerr <zachgould2004@gmail.com>
2024-07-03 17:30:21 -0400

    Create light control panel

--- SmartHomeGUI.java
+++ SmartHomeGUI.java
@@ -25,12 +25,45 @@ public class SmartHomeGUI {
         frame.setLayout(new BorderLayout()); // Layout style
 
         /*
-         * Create a panel
+         * Create main panel
          */
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // Add padding
         frame.add(mainPanel, BorderLayout.CENTER);
 
+        /*
+         * Create label for last action
+         */
+        confirmationLabel = new JLabel("No actions yet.");
+        confirmationLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(confirmationLabel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        /*
+         * Create Light Control panel
+         */
+        JPanel lightControlPanel = new JPanel();
+        lightControlPanel.setLayout(new BoxLayout(lightControlPanel, BoxLayout.Y_AXIS));
+        lightControlPanel.setBorder(BorderFactory.createTitledBorder("Light Control")); // Creates border with text
+
+        /*
+         * Create Light Control button
+         */
+        JButton toggleLightButton = new JButton("Toggle Light");
+        toggleLightButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        toggleLightButton.addActionListener(new ActionListener() {
+            private boolean lightsOn = false;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lightsOn = !lightsOn; // If lights are on, turn off, or vice versa
+                user.lightControl(lightsOn);
+            }
+        });
+        lightControlPanel.add(toggleLightButton);
+
+        mainPanel.add(lightControlPanel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
     }
 }
