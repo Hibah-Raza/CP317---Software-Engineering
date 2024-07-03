2dde5e698dddb06991e751684bde1df731654da2
kruugerr <zachgould2004@gmail.com>
2024-07-03 15:07:32 -0400

    Create Toggle Light button

--- SmartHomeGUI.java
+++ SmartHomeGUI.java
@@ -18,5 +18,27 @@ public class SmartHomeGUI {
         frame.setSize(400, 300);
         frame.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
+
+        /*
+         * Create a Toggle Light button
+         */
+        JButton toggleLightButton = new JButton("Toggle Light");
+        toggleLightButton.addActionListener(new ActionListener() {
+            private boolean lightsOn = false;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lightsOn = !lightsOn;
+                user.lightControl(lightsOn);
+            }
+        });
+
+        /*
+         * Positioning for Toggle Light button
+         */
+        gbc.gridx = 0; // Position in first column
+        gbc.gridy = 0; // Position in first row
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        frame.add(toggleLightButton, gbc); // Add buttong to GUI with these constraints
     }
 }
