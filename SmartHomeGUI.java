f7135896f65713d18be7ea698642b4e52214f8af
kruugerr <zachgould2004@gmail.com>
2024-07-03 13:13:07 -0400

    Initialise GUI Screen

--- SmartHomeGUI.java
+++ SmartHomeGUI.java
@@ -1,3 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SmartHomeGUI {
-    // class made
+    public SmartHomeGUI(User user) {
+        /*
+         * Debug Statement
+         */
+        System.out.println("Initializing Smart Home System...");
+
+        /*
+         * Initialise GUI screen
+         */
+        JFrame frame = new JFrame("Smart Home Automation Interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+    }
 }
